5fe6a729a8e15d29e46727a7c689c138c3b46036
Naman Agrawal <namanalways@gmail.com>
2025-11-21 12:59:03 +0530

    changes for 1st and 15 t nov attendance making as absent

--- custom_app/overrides/attendance.py
+++ custom_app/overrides/attendance.py
@@ -4,6 +4,19 @@ from custom_app.utils.saturday_utils import is_first_third_fifth_saturday
 
 class CustomAttendance(Document):
     def validate(self):
+        # Apply only on 1st / 3rd / 5th Saturdays
         if is_first_third_fifth_saturday(self.attendance_date):
-            self.status = "Absent"  # use allowed status of absent and mark weekly off
+
+            # Companies that should NOT be touched
+            allowed_companies = [
+                "Centre for Developmental Education",
+                "Vijaybhoomi University"
+            ]
+
+            # If company is allowed → do nothing (skip)
+            if self.company in allowed_companies:
+                return
+
+            # If company is NOT allowed → mark Absent
+            self.status = "Absent"
             self.custom_weekly_off_marker = "Weekly Off (Auto)"
